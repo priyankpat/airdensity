0074369f8ddc0bab7c4518525b73661e9ea16114
PropeReferio <lemuel.b.stevens@gmail.com>
2020-02-12 14:35:41 -0600

    Dry Air Formula Working

--- src/app/dailyda/dailyda.component.ts
+++ src/app/dailyda/dailyda.component.ts
@@ -10,16 +10,16 @@ export class DailydaComponent implements OnInit {
   weather;
   temp;
   press;
+  dry_da;
   //Crunch your numbers here, store it in a variable called result, etc.,
   //And in the template, {{ result }} will display that number.
   ISAT = 288.15;
-  ISAP = 29.92;
-  lapse_rate = 0.0065;
-  R = 8.3144598;
-  g = 9.80665;
-  M = 0.028964; // This is the molar mass of DRY air.
-  dry_da = this.ISAT/this.temp *(1 - ((this.press/this.ISAP)/(this.temp/this.ISAT))** ((this.lapse_rate*this.R)/(this.g*this.M - this.lapse_rate*this.R)))
-
+  ISAP = 1013.25;
+  expon = 0.234978134;
+  // lapse_rate = 0.0065;
+  // R = 8.3144598; Replaced all this with expon
+  // g = 9.80665;
+  // M = 0.028964; // This is the molar mass of DRY air.
 
 
   constructor(private weatherdataService: WeatherdataService) { }
@@ -27,10 +27,11 @@ export class DailydaComponent implements OnInit {
   ngOnInit() {
     this.weatherdataService.getWeather().subscribe((data)=>{
       console.log(data);
-      this.weather = data; //or ['main']??? But no, I think it's right
+      this.weather = data;
       this.temp = this.weather.main.temp;
       this.press = this.weather.main.pressure;
-      console.log(this.ISAT/this.temp *(1 - ((this.press/this.ISAP)/(this.temp/this.ISAT))** ((this.lapse_rate*this.R)/(this.g*this.M - this.lapse_rate*this.R))))
+      console.log(this.ISAT/0.0065 *(1 - ((this.press/this.ISAP)/(this.temp/this.ISAT))** (this.expon)))
+      this.dry_da = 3.28084 * this.ISAT/0.0065 *(1 - ((this.press/this.ISAP)/(this.temp/this.ISAT))** (this.expon))
     }
     )};
 
